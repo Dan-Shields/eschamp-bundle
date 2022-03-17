33403a2e2c4b2147b0279717afff21fe66814c71
Daniel Shields <d.shieldsuk@gmail.com>
2022-03-17 12:22:22 +0000

    feat: put AIRTABLE_API_KEY into .env

--- src/extension/airtable.ts
+++ src/extension/airtable.ts
@@ -3,24 +3,33 @@ import * as repDefaults from '../replicants'
 import Airtable from 'airtable'
 import _ from 'lodash'
 
-Airtable.configure({
-    apiKey: 'keyWv5e3wWx2sBVM6',
-})
+syncAirtable()
 
-const base = Airtable.base('appfSOqFLhZ2iq4GN')
+function syncAirtable() {
+    if (!process.env.AIRTABLE_API_KEY) {
+        nodecg().log.error('NO AIRTABLE API KEY CONFIGURED')
+        return
+    }
 
-const bots = nodecg().Replicant('bots', { defaultValue: repDefaults.bots })
+    Airtable.configure({
+        apiKey: process.env.AIRTABLE_API_KEY,
+    })
 
-setInterval(() => {
-    const newBots: typeof repDefaults.bots = {}
-    base('Bots')
-        .select()
-        .all((err, records) => {
-            records?.forEach((record) => {
-                newBots[record.id] = record.fields as repDefaults.Bot
+    const base = Airtable.base('appfSOqFLhZ2iq4GN')
+
+    const bots = nodecg().Replicant('bots', { defaultValue: repDefaults.bots })
+
+    setInterval(() => {
+        const newBots: typeof repDefaults.bots = {}
+        base('Bots')
+            .select()
+            .all((err, records) => {
+                records?.forEach((record) => {
+                    newBots[record.id] = record.fields as repDefaults.Bot
+                })
+                if (!err && !_.isEqual(newBots, bots.value)) {
+                    bots.value = newBots
+                }
             })
-            if (!err && !_.isEqual(newBots, bots.value)) {
-                bots.value = newBots
-            }
-        })
-}, 1000)
+    }, 1000)
+}
--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,5 +1,11 @@
+import path from 'path'
+import dotenv from 'dotenv'
 import type { NodeCG } from 'nodecg-types/types/server'
 import { set } from './nodecg-util'
+
+dotenv.config({
+    path: path.join(__dirname, '../.env'),
+})
 
 export = (nodecg: NodeCG) => {
     set(nodecg)

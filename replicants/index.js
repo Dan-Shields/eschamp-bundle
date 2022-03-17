de2b679da3c3d5f1cb6964a0704b9d0c33d4cf83
Daniel Shields <d.shieldsuk@gmail.com>
2022-03-17 12:43:38 +0000

    fix: add more build artifacts

---
+++ replicants/index.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.scoreboard = exports.bots = exports.timers = exports.ticker = void 0;
+exports.ticker = {
+    topText: '',
+    bottomText: '',
+    speed: 5,
+};
+exports.timers = {
+    nextMatchPlaying: false,
+    nextMatch: 300,
+};
+exports.bots = {};
+exports.scoreboard = {
+    bot1: {
+        id: '',
+        score: 0,
+    },
+    bot2: {
+        id: '',
+        score: 0,
+    },
+};

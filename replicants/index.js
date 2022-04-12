2ba95d238d4793c8ab7e79467c652beda6b1ec96
Daniel Shields <d.shieldsuk@gmail.com>
2022-04-12 17:25:00 +0100

    chore: rebuild

--- replicants/index.js
+++ replicants/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.introBot = exports.patrons = exports.scoreboard = exports.bots = exports.timers = exports.ticker = void 0;
+exports.gauntletBracket = exports.finalsBracket = exports.losersBracket = exports.mainBracket = exports.matches = exports.introBot = exports.patrons = exports.scoreboard = exports.bots = exports.timers = exports.ticker = void 0;
 exports.ticker = {
     topText: '',
     bottomText: '',
@@ -23,3 +23,32 @@ exports.scoreboard = {
 };
 exports.patrons = [];
 exports.introBot = null;
+exports.matches = Array.from({ length: 4 }, () => ({
+    bot1: null,
+    bot2: null,
+}));
+const generateBracketMatch = () => ({
+    bot1: {
+        id: null,
+        score: 0,
+    },
+    bot2: {
+        id: null,
+        score: 0,
+    },
+});
+exports.mainBracket = (() => {
+    const rounds = 4;
+    return Array.from({ length: rounds }, (_, index) => {
+        return Array.from({ length: Math.pow(2, rounds - 1 - index) }, () => generateBracketMatch());
+    });
+})();
+exports.losersBracket = Array.from({ length: 4 }, (_, index) => {
+    return Array.from({ length: index < 2 ? 4 : 2 }, () => generateBracketMatch());
+});
+exports.finalsBracket = Array.from({ length: 3 }, (_, index) => {
+    return Array.from({ length: index < 2 ? 1 : 2 }, () => generateBracketMatch());
+});
+exports.gauntletBracket = Array.from({ length: 7 }, () => [
+    generateBracketMatch(),
+]);
